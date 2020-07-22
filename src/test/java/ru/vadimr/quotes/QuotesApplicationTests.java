cc6882dd88f57f09ff4201dbcf8c9ae6ae277094
Vadim Rybakov <vadimio9217@gmail.com>
2020-07-22 08:39:01 +0300

    start

---
+++ src/test/java/ru/vadimr/quotes/QuotesApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.vadimr.quotes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuotesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
